5af52ff4ab6946a88fb538ed475556e0e8f0ecc0
Janarthanan2107 <131163725+Janarthanan2107@users.noreply.github.com>
2024-08-14 08:49:04 +0530

    Update navbar.component.jsx

--- src/components/navbar.component.jsx
+++ src/components/navbar.component.jsx
@@ -1,10 +1,12 @@
-import { useContext, useState } from "react"
+import { useContext, useEffect, useState } from "react"
 import logo from "../imgs/logo.png"
 import { Link, useNavigate } from "react-router-dom"
 import { UserContext } from "../App"
 import UserNavigationPanel from "./user-navigation.component"
+import axios from "axios"
+import { domain } from "../constants/domain"
 const Navbar = () => {
-    const { userAuth: { access_token, profile_img } } = useContext(UserContext)
+    const { userAuth, userAuth: { access_token, profile_img, new_notification_available }, setUserAuth } = useContext(UserContext)
     // state
     const [searchBarVisibility, setSearchBarVisibility] = useState(false)
     const [userNavPanel, setUserNavPanel] = useState(false)
@@ -34,12 +36,28 @@ const Navbar = () => {
         }
     }
 
+    useEffect(() => {
+        if (access_token) {
+            axios.get(domain + "/blog/notify/new-notification", {
+                headers: {
+                    'Authorization': `Bearer ${access_token}`
+                }
+            }).then(({ data }) => {
+                console.log(data);
+                setUserAuth({ ...userAuth, ...data })
+            }).catch((err) => {
+                console.log(err);
+            })
+        }
+    }, [access_token])
+
     return (
         <nav className="navbar z-50">
             {/* logo */}
             <Link to={"/"} className="flex-none w-10">
                 <img src={logo} alt="logo" />
             </Link>
+
 
             {/* sign in && sign up && write && search bar*/}
             <div className={`absolute bg-white w-full left-0 top-full mt-0 border-b border-grey py-4 px-[5vw] md:border-0 md:block md:relative md:inset-0 md:p-0 md:w-auto md:show ${searchBarVisibility ? "show" : "hide"}`}>
@@ -71,6 +89,9 @@ const Navbar = () => {
                             <Link to={"/dashboard/notification"}>
                                 <button className="w-12 h-12 rounded-full bg-grey relative hover:bg-black/10">
                                     <i className="fi fi-rr-bell text-2xl block mt-1"></i>
+                                    {new_notification_available &&
+                                        <span className="bg-red w-3 h-3 rounded-full absolute z-10 top-2 right-2"></span>
+                                    }
                                 </button>
                             </Link>
 
